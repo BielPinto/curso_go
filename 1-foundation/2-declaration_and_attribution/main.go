7d5d7b24fe88b2285358e1a3cb4f8852c967f812
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-06-22 14:01:32 -0300

    info: declaration and attribution

---
+++ 1-foundation/2-declaration_and_attribution/main.go
@@ -0,0 +1,19 @@
+package main
+
+const a = "Hello  world"
+
+var (
+	b bool
+	c int
+	d string
+	e float64
+)
+
+func main() {
+	a := "X"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+}
