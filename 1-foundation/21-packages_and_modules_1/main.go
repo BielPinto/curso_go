d56c7569b3ace63a69f67e553a7e9d3e6ed22b25
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-06-22 22:10:40 -0300

    info: packages and modules

---
+++ 1-foundation/21-packages_and_modules_1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"matematica"
+)
+
+func main() {
+
+	s := matematica.Sum(10, 20)
+	fmt.Println("Result: %v", s)
+}
---
+++ 1-foundation/21-packages_and_modules_1/mathematics/mathematics.go
@@ -0,0 +1,5 @@
+package mathematics
+
+func sum[T int | float64](a, b T) T {
+	return a + b
+}
