188df564248074720c3388e56a6e0ceb928056e6
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-07-10 00:21:46 -0300

    info: start client.go

---
+++ 0-fullcycle_challenges/1-Client-Server-API/client/main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+-Client.go must make an HTTP request to server.go requesting the dollar quote.
+
+- pply the context on the client and a 300 ms timeout of the server.go response
+
+- save the current quote in a file "quotacao.txt" in Dollar format:{value}
+
+- 3 context return the log error if the execution time is insufficient
+*/
+func main() {}
