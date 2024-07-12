54f7234a93343bb8da0b47b1945d6bd05c8355d2
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-07-12 00:21:37 -0300

    feat: adjusted table name for quotation and a rule if the table already exists

--- 0-fullcycle_challenges/1-Client-Server-API/server/db_quotation/init.sql
+++ 0-fullcycle_challenges/1-Client-Server-API/server/db_quotation/init.sql
@@ -1,7 +1,20 @@
 
 
 USE goexpert ;
-CREATE TABLE products (id varchar(255), code varchar(80), codein varchar(80), name varchar(80), high  decimal(10,4), low decimal(10,4), varBid decimal(10,4), PctChange decimal(10,2), Bid decimal(10,4), Ask decimal(10,4), CreateDate varchar(80), primary key (id));
+CREATE TABLE IF NOT EXISTS  quotation (
+  id varchar(255),
+  code varchar(80),
+  codein varchar(80),
+  name varchar(80),
+  high  decimal(10,4),
+  low decimal(10,4),
+  varBid decimal(10,4),
+  PctChange decimal(10,2),
+  Bid decimal(10,4),
+  Ask decimal(10,4),
+  CreateDate varchar(80),
+  primary key (id)
+);
 -- ALTER DATABASE device_commander OWNER TO device_commander;
 
 
