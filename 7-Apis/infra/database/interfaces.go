99c83e4888cd70aef650d50097b49fdae6862a8d
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-08-13 23:14:40 -0300

    feat: Creating main productDB methods

--- 7-Apis/infra/database/interfaces.go
+++ 7-Apis/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInteface interface {
 	Creat(User *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type ProductIterface interface {
+	Creat(product *entity.Product) error
+	FindAll(paga, limit int, sort string) ([]entity.Product, error)
+	UpdateById(id string) (*entity.Product, error)
+	update(product *entity.Product) error
+	Delete(id string) error
+}
