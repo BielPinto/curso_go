57344aa11a5d23ee9dbd482eae73795ad2241596
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-08-18 16:35:55 -0300

    info: finishing prouct DB testing

--- 7-Apis/infra/database/product_db_test.go
+++ 7-Apis/infra/database/product_db_test.go
@@ -11,12 +11,19 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestCreatNewProduct(t *testing.T) {
+func StartGorm() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		return db, err
+	}
+	db.AutoMigrate(&entity.Product{})
+	return db, nil
+}
+func TestCreatNewProduct(t *testing.T) {
+	db, err := StartGorm()
 	if err != nil {
 		t.Error(err)
 	}
-	db.AutoMigrate(&entity.Product{})
 
 	product, err := entity.NewProduct("Product 1 ", 10.00)
 	assert.NoError(t, err)
@@ -28,11 +35,10 @@ func TestCreatNewProduct(t *testing.T) {
 }
 
 func TestFinalALLProducts(t *testing.T) {
-	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	db, err := StartGorm()
 	if err != nil {
 		t.Error(err)
 	}
-	db.AutoMigrate(&entity.Product{})
 
 	for i := 1; i < 24; i++ {
 		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
@@ -59,4 +65,54 @@ func TestFinalALLProducts(t *testing.T) {
 	assert.Equal(t, "Product 21", products[0].Name)
 	assert.Equal(t, "Product 23", products[2].Name)
 
+}
+
+func TestFindProductByID(t *testing.T) {
+	db, err := StartGorm()
+	if err != nil {
+		t.Error(err)
+	}
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 1", product.Name)
+}
+
+func TestUpdateProduct(t *testing.T) {
+
+	db, err := StartGorm()
+	if err != nil {
+		t.Error(err)
+	}
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product.Name = "Product 2"
+	err = productDB.Update(product)
+	assert.NoError(t, err)
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 2", product.Name)
+
+}
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := StartGorm()
+	if err != nil {
+		t.Error(err)
+	}
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	_, err = productDB.FindByID(product.ID.String())
+	assert.Error(t, err)
 }
