d55b6a3a7509d1d66b4ece3958c291ec2b4d9fa5
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-08-18 20:29:45 -0300

    fix: fix  function nomenclature

--- 7-Apis/infra/database/interfaces.go
+++ 7-Apis/infra/database/interfaces.go
@@ -3,14 +3,14 @@ package database
 import "github.com/BielPinto/curso_go/7-Apis/internal/entity"
 
 type UserInteface interface {
-	Creat(User *entity.User) error
+	Create(User *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
 
 type ProductIterface interface {
-	Creat(product *entity.Product) error
+	Create(product *entity.Product) error
 	FindAll(paga, limit int, sort string) ([]entity.Product, error)
-	UpdateById(id string) (*entity.Product, error)
-	update(product *entity.Product) error
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
 	Delete(id string) error
 }
