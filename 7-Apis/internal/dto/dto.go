25ab7f749a1baa27312d70b49fa95837e2b53f0b
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-08-18 22:45:52 -0300

    feat:  add func and rotuer to create user

--- 7-Apis/internal/dto/dto.go
+++ 7-Apis/internal/dto/dto.go
@@ -1,6 +1,12 @@
 package dto
 
-type CreatProductInput struct {
-	Name  string  `json:"name`
-	Price float64 `json:"price`
+type CreateProductInput struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type CreateUserInput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
