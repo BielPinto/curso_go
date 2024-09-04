946d4a071d5f57a77db29590db00727f518d7485
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-03 21:33:30 -0300

    info: added serve and challenge requirements

---
+++ 0-fullcycle_challenges/2-Multitheading/cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+          MultiThreading an APIs
+- Create Function to make request to the APIs.  --> pendent
+	- https://brasilapi.com.br/api/cep/v1/01153000 + cep --> pendent
+	- http://viacep.com.br/ws/" + cep + "/json/           --> pendent
+- Create funciton that saves the fastest reponse and discards the showest one.  --> pendent
+- Show the data , address and whitch Api sent it on the command line.  --> pendent
+- Limit the response time to 1 second, othewise  the timeout should be displayed. --> pendent
+*/
+
+func main() {
+
+}
