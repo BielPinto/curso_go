db89c9748d8fb90a0a9f83ccf30a6ff9b2067b8e
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-05 09:34:43 -0300

    feat: implementing the Dispatch method

--- 9-handling_events/pkg/events/event_dispatcher.go
+++ 9-handling_events/pkg/events/event_dispatcher.go
@@ -13,6 +13,15 @@ func NewEventDispatcher() *EventDispatcher {
 		handlers: make(map[string][]EventHandlerInterface),
 	}
 }
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+	return nil
+}
 
 func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
 	if _, ok := ed.handlers[eventName]; ok {
