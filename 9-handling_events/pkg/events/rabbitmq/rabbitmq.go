5aa9dcbf96464ef1616311484e111b31ccf43d24
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-09 17:39:06 -0300

    Feat: Creating consumers in practice

--- 9-handling_events/pkg/events/rabbitmq/rabbitmq.go
+++ 9-handling_events/pkg/events/rabbitmq/rabbitmq.go
@@ -4,7 +4,7 @@ import amqp "github.com/rabbitmq/amqp091-go"
 
 func OpenChannel() (*amqp.Channel, error) {
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://quest:quest@localhost:5672/")
 	if err != nil {
 		panic(err)
 	}
