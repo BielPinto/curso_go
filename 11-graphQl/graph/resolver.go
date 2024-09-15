29effdb52c57c304833bc53f125370d6050446a6
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-15 15:25:11 -0300

    feat: creating resolver for category

--- 11-graphQl/graph/resolver.go
+++ 11-graphQl/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/BielPinto/curso_go/11-graphQL/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	CategorDB *database.Category
+}
