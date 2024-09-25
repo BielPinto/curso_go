005f66f85360ecad0995397657f44aa3dab40b03
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-24 23:50:37 -0300

    feat: implemented CreateCategory

---
+++ 12-grpc/internal/services/category.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"context"
+
+	"github.com/BielPinto/curso_go/12-grpc/internal/database"
+	"github.com/BielPinto/curso_go/12-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryResquest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+
+}
