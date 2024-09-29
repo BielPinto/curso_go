f9426299a19d4b70317e70ea9cf86eda01d09aa4
Gabriel Rocha <gabrielp.roch@hotmail.com>
2024-09-29 12:53:23 -0300

    feat: creating gRPC Server

---
+++ 12-grpc/cmd/grpcServe/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/BielPinto/curso_go/12-grpc/internal/database"
+	"github.com/BielPinto/curso_go/12-grpc/internal/pb"
+	"github.com/BielPinto/curso_go/12-grpc/internal/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	categoryDb := database.NewCategory(db)
+	categoryService := services.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+
+}
